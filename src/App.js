129f4bbd3656411998fb592a0e5f267f93aeeb6d
kazuko takasaki <kazukotakasaki@kazukonoMacBook-Air.local>
2021-05-28 21:35:50 +0900

    検索機能

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]),
+        [text, setText] = useState(''),
+        [query, setQuery] = useState('japan');
+
+  useEffect ( () => {
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setImages(data.results)
+      })
+  },[query])
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      setQuery(text);
+      setText('');
+      console.log(text)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <form onSubmit={onSubmit}>
+          <input
+          type='text'
+          onChange= {e => setText(e.target.value)}
+          value = {text}
+          />
+          <button type='submit'>
+            search
+          </button>
+        </form>
+      </div>
+      <div className='container'>
+        {
+          images.map(image => (
+            <div key={image.id} className='card'>
+              <img src={image.urls.regular} className='card-img' alt=''/>
+              <div className='card-content'>
+                <div className='card-title'>
+                  {image.alt_description}
+                </div>
+              </div>
+            </div>   
+          ))
+        }
+      </div>
     </div>
   );
 }
